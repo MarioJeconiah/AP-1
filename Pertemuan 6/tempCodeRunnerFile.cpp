b90e8a737e20ed94279c2069704a7b2f9805df99
MarioJeconiah <mariopurba40@gmail.com>
2025-05-25 16:32:20 +0700

    // Menggunakan namespace agar tidak perlu tulis std :: pada penggunaan library

--- Pertemuan 6/tempCodeRunnerFile.cpp
+++ Pertemuan 6/tempCodeRunnerFile.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // header untuk c++
+using namespace std; // Menggunakan namespace agar tidak perlu tulis std :: pada penggunaan library 
 
-void sapa(string nama) {
-    cout << "Halo" << nama << "! Selamat belajar C++" << endl;
+void sapa(string nama) { // function sapa tipe data void dengan parameter formal string nama
+    cout << "Halo" << nama << "! Selamat belajar C++" << endl; // menampilkan output
 }
 
 
-int main(){
-    string namaPengguna = "Mario";
+int main(){ // mulai program utama
+    string namaPengguna = "Mario"; // inisialisasi variabel namaPengguna tipe data string
 
-    sapa(namaPengguna);
+    sapa(namaPengguna); // menggunakan function sapa dengan parameter aktual namaPengguna
 
-    return 0;
+    return 0; // mengembalikan nilai dan program selesai
 }
