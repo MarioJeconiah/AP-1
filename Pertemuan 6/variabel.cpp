ea8a1a907080cfebe3bee10cc9f264687cfee379
MarioJeconiah <mariopurba40@gmail.com>
2025-05-25 16:35:03 +0700

    Pertemuan 6 : Fungsi dan Header

--- Pertemuan 6/variabel.cpp
+++ Pertemuan 6/variabel.cpp
@@ -1,23 +1,23 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // header untuk c++
+using namespace std; // Menggunakan namespace agar tidak perlu tulis std :: pada penggunaan library 
 
-string namaGLobal = "Ilmu";
+string namaGLobal = "Ilmu"; // inisialisasi variabel namaGLobal tipe data string
 
-void namaVariabel(){
-    string namaLokal = "Komputer";
-
-    // coba akses
-    cout << namaLokal << endl; 
+void namaVariabel(){ // function namaVariabel tipe data void
+    string namaLokal = "Komputer"; // inisialisasi variabel namaLokal tipe data string 
 
     // coba akses
-    cout << namaGLobal << endl;
+    cout << namaLokal << endl; // menampilkan output
+
+    // coba akses
+    cout << namaGLobal << endl; // menampilkan output
 }
 
-int main(){
-    namaVariabel();
+int main(){ // mulai program utama
+    namaVariabel(); // memanggil function namaVariabel
 
     // coba akses
-    cout << namaGLobal << endl;
+    cout << namaGLobal << endl; // menampilkan output
     
     // coba akses
     // cout << namaLokal << endl;    ga iso
