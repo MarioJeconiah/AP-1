4af67c2db0af2add38baa45575e07221620c9e03
MarioJeconiah <mariopurba40@gmail.com>
2025-05-25 14:05:48 +0700

    Pertemuan 1 : Pengenalan & Operasi Input & Output

--- Pertemuan 1/BelajarC.cpp
+++ Pertemuan 1/BelajarC.cpp
@@ -1,38 +1,38 @@
 #include <stdio.h> // header untuk C
-#include <conio.h>
+#include <conio.h> // header untuk fungsi yang akan dipakai seperti getch
 
-int main() {
-    char nama[50];
-    int nim;
-    char kom[2];
-    float ip;
+int main() { // mulai program utama
+    char nama[50]; // inisialisasi variabel nama bertipe data char sebanyak 49
+    int nim; // inisialisasi variabel nim bertipe data int
+    char kom[2]; // inisialisasi variabel kom bertipe data char sebanyak 2
+    float ip; // inisialisasi variabel ip bertipe data float
 
     printf("Hello World\n"); // fungsi keluaran pada C
 
-    printf("Masukkan nama : ");
+    printf("Masukkan nama : "); //menampilkan output
     gets(nama); // get string
 
-    printf("Masukkan nim : ");
+    printf("Masukkan nim : "); // menampilkan output
     scanf("%d", &nim); // fungsi masukan pada C
 
-    getchar();
+    getchar(); // fungsi untuk menerima inputan char
 
-    printf("Masukkan kom : ");
-    gets(kom); 
+    printf("Masukkan kom : "); // menampilkan output
+    gets(kom); // menerima inputan kom
 
-    printf("Masukkan ip : ");
-    scanf("%f", &ip); 
+    printf("Masukkan ip : "); // menampilkan output
+    scanf("%f", &ip); // menerima inputan ip
 
     /* Untuk Output */
-    printf("Nama : ");
+    printf("Nama : "); // menampilkan output
     puts(nama); // put string, digunakan untuk string dalam bentuk array char
 
-    printf("NIM : %d\n", nim);
+    printf("NIM : %d\n", nim); // menampilkan output
 
-    printf("KOM : ");
-    puts(kom); 
-    printf("IP : %f\n", ip);
+    printf("KOM : "); // menampilkan output
+    puts(kom); // put string, untuk menampilkan output sebagai string
+    printf("IP : %f\n", ip); // menampilkan output
 
-    printf("press any button to continue...");
-    getch(); 
+    printf("press any button to continue..."); // menampilkan output
+    getch(); // menerima inputan char
 }
