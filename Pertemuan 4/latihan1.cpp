986da386ad5d6ee15fedfd115c5f6884a0658e6a
MarioJeconiah <mariopurba40@gmail.com>
2025-05-25 14:57:10 +0700

    Pertemuan 4 : Perulangan

--- Pertemuan 4/latihan1.cpp
+++ Pertemuan 4/latihan1.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
+#include <iostream> // header untuk c++
 
-using namespace std;
+using namespace std; // Menggunakan namespace agar tidak perlu tulis std :: pada penggunaan library 
 
-int main(){
-    string kalimat;
-    int i;
+int main(){ // mulai program
+    string kalimat; // inisialisasi variabel kalimat tipe data string
+    int i; // inisialisasi variabel i tipe data integer
 
-    cout << "Masukkan kalimat : ";
-    getline(cin, kalimat);
+    cout << "Masukkan kalimat : "; // menampilkan output
+    getline(cin, kalimat); // membaca 1 baris inputan dan disimpan di variabel kalimat
 
     
-    for (i = 0; i < kalimat.length(); i++) {
-        kalimat[i] = toupper(kalimat [i]);
+    for (i = 0; i < kalimat.length(); i++) {  // pengulangan sebanyak panjang karakter dari inputan string
+        kalimat[i] = toupper(kalimat [i]); //mengubah karakter mulai dari awal sampai akhir menjadi huruf besar
     }
 
-    cout << "Kalimat dalam hruuf kapital : " << kalimat << endl;
+    cout << "Kalimat dalam huruf kapital : " << kalimat << endl; // menampilkan output
 
-    return 0;
+    return 0; // mengembalikan nilai dan program selesai
 }
