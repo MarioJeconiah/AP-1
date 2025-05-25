cdc878ff1b7047ad04e43e69eeef3372027a6734
MarioJeconiah <mariopurba40@gmail.com>
2025-05-25 14:33:34 +0700

    Pertemuan 3 : Operasi Kondisi

--- Pertemuan 3/Latihan.cpp
+++ Pertemuan 3/Latihan.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // header untuk c++
+using namespace std; // Menggunakan namespace agar tidak perlu tulis std :: pada penggunaan library     
 
-int main(){
-    int nilai;
+int main(){ // mulai program
+    int nilai; // inisialisasi variabel nilai bertipe data integer
 
-    cin >> nilai;
+    cin >> nilai; // menerima inputan dan disimpan di variabel nilai
 
-    if (nilai % 10 == 0){
-        cout << "Habis dibagi 10";
+    if (nilai % 10 == 0){ // pengkondisian jika sisa bagi nilai dengan 10 adalah 0 maka jalankan instruksi
+        cout << "Habis dibagi 10"; // menampilkan output
     }
 
-    else if (nilai % 5 == 0){
-        cout <<"Habis dibagi 5";
+    else if (nilai % 5 == 0){ // jika pengkondisian sebelumnya salah lanjut ke pengkondisian baru yang dimana jika sisa bagi nilai dengan 5 adalah 0 maka jalankan instruksi
+        cout <<"Habis dibagi 5"; // menampilkan output
     }
 
-    else {
-        cout <<"tidak habis dibagi 5 atau 10";
+    else { // jika semua pengkondisian salah sebelumnya jalankan instruksi berikut
+        cout <<"tidak habis dibagi 5 atau 10"; // menampilkan output
     }
 }
