05b8e05c5056a70717ed1b3ded3dcc293c4a87a8
MarioJeconiah <mariopurba40@gmail.com>
2025-05-25 16:18:16 +0700

    Pertemuan 6 : Fungsi dan Header

--- Pertemuan 6/BelajarCPP.cpp
+++ Pertemuan 6/BelajarCPP.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // header untuk c++
+using namespace std; // Menggunakan namespace agar tidak perlu tulis std :: pada penggunaan library 
 
-void sapa(string nama) {
-    cout << "Halo " << nama << "! Selamat belajar C++" << endl;
+void sapa(string nama) { // membuat function baru bertipe data void dengan parameter formal nama tipe data string
+    cout << "Halo " << nama << "! Selamat belajar C++" << endl; // menampilkan output
 }
 
 
-int main(){
-    string namaPengguna = "Mario";
+int main(){ // mulai program utama
+    string namaPengguna = "Mario"; // inisialisasi variabel namaPengguna dengan tipe data string 
 
-    sapa(namaPengguna);
+    sapa(namaPengguna); // menjalankan function sapa dengan parameter aktual namaPengguna
 
-    return 0;
+    return 0; // mengembalikan nilai dan program selesai
 }
