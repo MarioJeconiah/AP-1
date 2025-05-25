da446dfc69bae2cae65f3898525d2c268f7abcb3
MarioJeconiah <mariopurba40@gmail.com>
2025-05-25 13:57:20 +0700

    Pertemuan 1 : Pengenalan & Operasi Input & Output

--- Pertemuan 1/Belajar.cpp
+++ Pertemuan 1/Belajar.cpp
@@ -1,41 +1,41 @@
 #include <iostream> // header untuk c++
 #include <conio.h> // header untuk getche() dan getch()
-using namespace std;
+using namespace std; // Menggunakan namespace agar tidak perlu tulis std :: pada penggunaan library
 
-int main () {
+int main () { // program utama
 
-    string nama;
-    char kom, jenisKelamin;
-    int nim;
-    float ip;
+    string nama; // inisialisasi variabel nama bertipe data string
+    char kom, jenisKelamin; // inisialisasi variabel kom dan jenisKelamin tipe data char
+    int nim; // inisialisasi variabel nim bertipe data integer
+    float ip; // inisialisasi variabel ip bertipe data float
     
     /* ini untuk komentar 
     beberapa baris */
 
-    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl; //menampilkan output
 
-    cout << "Masukkan nama : ";
-    // cin >> nama;
-    getline(cin, nama); // agar karakter spasi bisa terbaca
+    cout << "Masukkan nama : "; // menampilkan output
+    // cin >> nama; // membaca input dan disimpan di variabel nama
+    getline(cin, nama); // agar karakter spasi bisa terbaca dan membaca input yang akan disimpan di variabel nama
 
-    cout << "Masukkan KOM : ";
-    cin >> kom;
+    cout << "Masukkan KOM : "; // menampilkan output
+    cin >> kom; // membaca input dan disimpan di variabel kom
 
-    cout << "Masukkan NIM : ";
-    cin >> nim;
+    cout << "Masukkan NIM : "; // menampilkan output
+    cin >> nim; // membaca input dan disimpan di variabel nim
 
-    cout << "Masukkan IP : ";
-    cin >> ip;
+    cout << "Masukkan IP : "; // menampilkan output 
+    cin >> ip; // membaca input dan disimpan di variabel ip
 
-    cout << "Masukkan jenis kelamin (L/P) : ";
-    jenisKelamin = getche(); // agar karakter langsung tampil, jadi ga perlu tekan enter
+    cout << "Masukkan jenis kelamin (L/P) : "; // menampilkan output
+    jenisKelamin = getche(); // agar karakter langsung tampil, jadi ga perlu tekan enter dan membaca input serta menyimpan input di variabel jenisKelamin
 
     /* untuk output */
-    cout << "\n" << nama << endl;
-    cout << kom << endl;
-    cout << nim << endl;
-    cout << ip << endl;
-    putchar(jenisKelamin); // untuk menampilkan karakter jenis kelamin
+    cout << "\n" << nama << endl; // menampilkan output
+    cout << kom << endl; // menampilkan output
+    cout << nim << endl; // menampilkan output
+    cout << ip << endl; // menampilkan output
+    putchar(jenisKelamin); // untuk menampilkan karakter jenis kelamin 
 
     getch(); // karakter yang diketik ga ditampilkan di layar
 }
