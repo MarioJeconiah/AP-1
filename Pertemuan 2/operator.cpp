ea91dd856fe06e6d19424d5d65a0c2fc5a1f24ab
MarioJeconiah <mariopurba40@gmail.com>
2025-05-25 14:30:13 +0700

    Pertemuan 2 : Penggunaan Operator

--- Pertemuan 2/operator.cpp
+++ Pertemuan 2/operator.cpp
@@ -1,23 +1,23 @@
-#include <iostream>
+#include <iostream> // header untuk c++
 
-using namespace std;
+using namespace std; // Menggunakan namespace agar tidak perlu tulis std :: pada penggunaan library
 
-int main(){
-    int a, b;
+int main(){ // mulai program utama
+    int a, b; // menginisialisasi variabel a dan b bertipe data integer
 
-    system("CLS");
+    system("CLS"); // membersihkan terminal
 
     //Assignment Operator (=)
-    a = 3;
-    b = 5;
+    a = 3; // memberi nilai variabel a adalah 3
+    b = 5; // memberi nilai variabel b adalah 5
 
     //Aritmethical Operator (+, -, /, *, %)
-    /* int tambah = a + b;
-    int kurang = a - b;
-    int kali = a * b;
+    /* int tambah = a + b; inisialisasi variabel tambah tipe data integer dan nilai adalah variabel a + b
+    int kurang = a - b; inisialisasi variabel kurang tipe data integer dan nilai adalah variabel a - b 
+    int kali = a * b; inisialisasi variabel kali tipe data integer dan nilai adalah variabel a * b 
     float bagi = (float) a / (float) b; // tipe casting mengubah tipe data dari sebuah variabel
-    int modulo = a % b;
-
+    int modulo = a % b; // inisialisasi variabel modulo tipe data integer dengan nilai adalah sisa bagi a dengan b
+    // menampilkan output
     cout << "Hasil penjumlahan : " << tambah << endl;
     cout << "Hasil pengurangan : " << kurang << endl;
     cout << "Hasil perkalian : " << kali << endl;
@@ -25,7 +25,7 @@ int main(){
     cout << "Hasil sisa bagi : " << modulo << endl;*/
 
     // Relational Operator
-
+    // menampilkan output
     /*cout << (a == b) << endl;
     cout << (a > b) << endl;
     cout << (a >= b) << endl;
@@ -33,8 +33,8 @@ int main(){
     cout << (a <= b) << endl;
     cout << (a != b) << endl; */
 
-    // Logical Operator (&&, ||, !!)
-    /*cout << (true && true) << endl;
+    // Logical Operator (&&, ||, !!) dan menampilkan output berdasarkan hasil operasi logic
+    /*cout << (true && true) << endl; 
     cout << (true && false) << endl;
     cout << (false && true) << endl;
     cout << (false && false) << endl; */
@@ -47,7 +47,7 @@ int main(){
     /* cout << !true << endl;
     cout << !false << endl; */
 
-    // Bitwise Operator (&, |, ~, ^, <<, >>) --> Operasi Biner
+    // Bitwise Operator (&, |, ~, ^, <<, >>) --> Operasi Biner dan menampilkan output berdasarkan hasil operasi bitwise
     /* cout << (5 & 7) << endl;
     cout << (5 | 7) << endl;
     cout << (5 ^ 7) << endl;
@@ -55,50 +55,50 @@ int main(){
     cout << (7 << 2) << endl;
     cout << (7 >> 2) << endl; */
 
-    // Shorthand
+    // Shorthand dan menambahkan nilai ke variabel
     // a += 7 // a = a + 7
     /* a += 7;
     cout << a << endl; 
 
-    a -= 7;
-    cout << a << endl;
+    a -= 7; mengurangkan nilai ke variabel
+    cout << a << endl; menampilkan output
 
-    a *= 7;
-    cout << a << endl;
+    a *= 7; mengalikan nilai ke variabel
+    cout << a << endl; menampilkan output
 
-    a /= 7;
-    cout << a << endl; */
+    a /= 7; membagikan nilai ke variabel
+    cout << a << endl; menampilkan output */ 
 
     // Increment % Decrement
     // Pre Increment 
-    /* cout << a << endl;
-    cout << ++a << endl;
+    /* cout << a << endl; menampilkan output
+    cout << ++a << endl; output ketika a pre increment
 
-    cout << b << endl;
-    cout << ++b << endl; */
+    cout << b << endl; menampilkan output
+    cout << ++b << endl; output ketika b pre increment */
 
     // Post Increment
-    /* cout << a << endl;
-    cout << a++ << endl;
-    cout << a << endl;
+    /* cout << a << endl; menampilkan output
+    cout << a++ << endl; output ketika a post increment
+    cout << a << endl; menampilkan nilai output a yang bertambah 1
     
-    cout << b << endl;
-    cout << b++ << endl;
-    cout << b << endl; */
+    cout << b << endl; menampilkan output
+    cout << b++ << endl; output ketika b post increment
+    cout << b << endl; menampilkan nilai output b yang bertambah 1 */ 
 
     //Pre Decrement
-    /* cout << a << endl;
-    cout << --a << endl;
+    /* cout << a << endl; menampilkan output
+    cout << --a << endl; menampilkan nilai ketika a pre decrement
 
-    cout << b << endl;
-    cout << --b << endl; */
+    cout << b << endl; menampilkan output
+    cout << --b << endl; menampilkan nilai ketika b pre decrement*/
 
     // Post Decrement
-    cout << a << endl;
-    cout << a-- << endl;
-    cout << a << endl;
+    cout << a << endl; // menampilkan output
+    cout << a-- << endl; // menampilkan output ketika a post decrement
+    cout << a << endl; // menampilkan nilai output a yang berkurang 1
     
-    cout << b << endl;
-    cout << b-- << endl;
-    cout << b << endl;
+    cout << b << endl; // menampilkan output
+    cout << b-- << endl; // menampilkan output ketika b post decrement
+    cout << b << endl; // menampilkan nilai output b yang berkurang 1
 } 
