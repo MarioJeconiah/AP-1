ea61cf1a03db244be1571a0876ed4dfc4390fa5a
MarioJeconiah <mariopurba40@gmail.com>
2025-05-25 16:15:35 +0700

    Pertemuan 5 : Array & Vector

--- Pertemuan 5/vektor.cpp
+++ Pertemuan 5/vektor.cpp
@@ -1,29 +1,29 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+#include <iostream> // header untuk c++
+#include <vector> // header untuk vector
+using namespace std; // Menggunakan namespace agar tidak perlu tulis std :: pada penggunaan library 
 
-int main () {
+int main () { // mulai program utama
     // Vector Declaration & Initialization
 
-    vector<string> nama_karyawan = {"Alya", "Parul", "Syukron", "Aurick", "Dzakwan"};
+    vector<string> nama_karyawan = {"Alya", "Parul", "Syukron", "Aurick", "Dzakwan"}; // inisialisasi variabel vector nama_karyawan tipe data string 
 
-    // for (string karyawan : nama_karyawan) {
-    //     cout << karyawan << endl;
+    // for (string karyawan : nama_karyawan) { pengulangan sebanyak jumlah elemen pada variabel vector nama_karyawan
+    //     cout << karyawan << endl; tampilkan output
     // }
 
     // Add data to Vector
-    nama_karyawan.push_back("Dhafa");
+    nama_karyawan.push_back("Dhafa"); // menambahkan data ke variabel vektor nama_karyawan di index paling terakhir
 
-    // for (int i = 0; i < nama_karyawan.size(); i++){
-    //     cout << nama_karyawan[i] << endl;
+    // for (int i = 0; i < nama_karyawan.size(); i++){ pengulangan sebanyak ukuran elemen vektor nama_karyawan
+    //     cout << nama_karyawan[i] << endl; tampilkan output
     // }
 
     // Delete data from Vector
-    nama_karyawan.pop_back(); 
+    nama_karyawan.pop_back();  // menghapus elemen terakhir pada variabel vektor nama_karyawan
 
-    nama_karyawan.erase(nama_karyawan.begin() + 3);
+    nama_karyawan.erase(nama_karyawan.begin() + 3); // menghapus elemen keempat pada variabel vektor nama karyawan
 
-    for (int i = 0; i < nama_karyawan.size(); i++){
-        cout << nama_karyawan[i] << endl;
+    for (int i = 0; i < nama_karyawan.size(); i++){ // pengulangan sebanyak jumlah elemen pada variabel vektor nama_karyawan
+        cout << nama_karyawan[i] << endl; // tampilkan output 
     }
 }
