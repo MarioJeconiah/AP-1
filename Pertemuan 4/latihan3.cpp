896fdd32e15ab28c4dfca4c15c3f38f12be00e8a
MarioJeconiah <mariopurba40@gmail.com>
2025-05-25 15:03:08 +0700

    Pertemuan 4 : Perulangan

--- Pertemuan 4/latihan3.cpp
+++ Pertemuan 4/latihan3.cpp
@@ -1,15 +1,16 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // header untuk c++
+using namespace std; // Menggunakan namespace agar tidak perlu tulis std :: pada penggunaan library 
 
-int main(){
-    string kalimat;
-    cout << "Masukkan kalimat: ";
-    getline(cin, kalimat);
+int main(){ // mulai program
+
+    string kalimat; // inisialisasi variabel kalimat tipe data string
+    cout << "Masukkan kalimat: "; // menampilkan output
+    getline(cin, kalimat); // membaca 1 baris inputan dan disimpan di variabel kalimat
 
 
-    for (int i = 0; i < kalimat.length(); i++){
-        for (int j = 0; j <= i; j++){
-            cout << kalimat[j];
+    for (int i = 0; i < kalimat.length(); i++){ // pengulangan sebanyak panjang karakter dari inputan string
+        for (int j = 0; j <= i; j++){ // pengulangan sebanyak i + 1 kali
+            cout << kalimat[j]; // tampilkan karakter index ke j dari variabel kalimat
         }
     }
 }
