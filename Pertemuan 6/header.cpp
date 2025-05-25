8261f23968e35268bf44e166f68643f5e04a45cf
MarioJeconiah <mariopurba40@gmail.com>
2025-05-25 16:30:11 +0700

    Pertemuan 6 : Fungsi dan Header

--- Pertemuan 6/header.cpp
+++ Pertemuan 6/header.cpp
@@ -2,25 +2,25 @@
 #include <cmath> // bawaan
 #include "headerCustom.h" // header buatan sendiri
 
-u;
+u; // dari header dimana u adalah using namespace std;
 
-int jumlah (int a, int b){
-    adalah a + b;
+int jumlah (int a, int b){ // function jumlah dengan parameter formal integer a dan b
+    adalah a + b; // mengembalikan nilai a + b
 }
 
-int kurang (int a, int b){
-    adalah a - b;
+int kurang (int a, int b){ // function kurang dengan parameter formal integer a dan b
+    adalah a - b; // mengembalikan nilai a - b
 }
 
-int main(){
-    system("CLS");
-    int n;
+int main(){ // mulai program utama
+    system("CLS"); // bersihkan terminal
+    int n; // deklarasi variabel n bertipe data integer
 
-    o l jumlah(2, 3) l endl;
-    o l kurang (5, 3) l endl;
+    o l jumlah(2, 3) l endl; // menampilkan output 
+    o l kurang (5, 3) l endl; // menampilkan output
     
-    double angka = 25.0;
-    double akar = sqrt(angka);
-    o l "akar dari " l angka l " adalah " l akar l endl;
+    double angka = 25.0; // inisialisasi variabel angka tipe data double
+    double akar = sqrt(angka); // inisialisasi variabel akar tipe data double adalah fungsi sqrt dengan variabel angka sebagai parameter aktual
+    o l "akar dari " l angka l " adalah " l akar l endl; // menampilkan output
 
 }
